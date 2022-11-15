f81fed15432b494d0baec55bdb43b08e66c0a1ff
Samuel Braun <office.samigo.a@gmail.com>
2022-11-15 17:12:06 +0100

    Added awful authentication

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+
+### OAuth2 Setup
+* https://discord.com/developers
+* Create a new application
+* Go to the OAuth2 tab
+* Copy the Client ID and Client Secret and paste them into .env
+* Add the following redirect URL to the OAuth2 tab: http://localhost:3000/auth.html
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import dotenv from "dotenv";
+import path from 'path';
+import fetch from 'node-fetch';
+dotenv.config();
 
 const app = express();
 const config = {
@@ -10,3 +14,61 @@ app.listen(config.OVERLAY_PORT, () => {
     console.log(`Example app listening on http://localhost:${config.OVERLAY_PORT}`);
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/login.html"));
+});
+
+
+app.get('/mod', async (req, res) => {
+    const loggedIn = await isLoggedIn(req);
+    if (!loggedIn) {
+        console.log('Not logged in');
+        res.redirect('/login');
+        return;
+    }
+
+    res.sendFile(path.join(__dirname, '../../private/mod.html'));
+});
+
+
+app.get('/auth', async (req, res) => {
+    if (req.query.token_type == null || req.query.access_token == null) {
+        res.sendFile(path.join(__dirname, '../../private/auth.html'));
+        return;
+    }
+
+    res.redirect(`/mod?token_type=${req.query.token_type}&access_token=${req.query.access_token}`);
+});
+
+
+function isLoggedIn(req) {
+    return new Promise((resolve, reject) => {
+        const { token_type, access_token } = req.query;
+        if (token_type == null || access_token == null) {
+            resolve(false);
+            return;
+        }
+
+        console.log(`Checking if logged in with ${token_type} ${access_token}`);
+
+        fetch('https://discord.com/api/users/@me', {
+            headers: {
+                authorization: `${token_type} ${access_token}`,
+            },
+        })
+        .then(result => result.json())
+        .then(response => {
+            console.log(response);
+            const { username, discriminator } = response;
+            if (response.message !== '401: Unauthorized' && username != null && discriminator != null) {
+                console.log(`Authorized ${username}#${discriminator}`);
+                resolve(true);
+            }
+
+            resolve(false);
+        })
+        .catch(() => {
+            resolve(false);
+        });
+    });
+}
