d18ec22a5cf5b4b0d39ecf8791c160a2f28ec84d
Samuel Braun <office.samigo.a@gmail.com>
2022-11-24 20:25:59 +0100

    style: :art: Refactored UI communication (#8)

--- src/UIWebSocketManager.ts
+++ src/UIWebSocketManager.ts
@@ -12,54 +12,21 @@ export default class UIWebSocketManager {
 
     onMessage(message: RawData) {
         const data = JSON.parse(message.toString());
-        this.sendToUI(100, data);
 
         switch(data.command) {
-            case "update-scoresabers":
-                this.updateScoresabers(data.ids).then(scoresabers => this.sendToUI(100, {
-                    command: "update-scoresabers",
-                    scoresabers,
-                }));
+            case "update-lead":
+                this.sendToUI("lead-update", {
+                    leftLead: data.leftLead,
+                    rightLead: data.rightLead,
+                });
+                break;
         }
     }
     
 
-    sendToUI(type: number, data: Array<any> | object) {
+    sendToUI(type: string, data: Array<any> | object) {
         if (this.socket.readyState === WebSocket.OPEN) {
-            this.socket.send(JSON.stringify({ Type: type, ...data }));
-        }
-    }
-
-    
-    async updateScoresabers(ids: string[]) {
-        for (const player of ids) {
-            if (this.scoresabers[player] == null) {
-                const scoresaber = await this.getScoresaber(player);
-                this.scoresabers[player] = scoresaber;
-            }
-        }
-        return this.scoresabers;
-    }
-
-
-    async getScoresaber(playerId: string): Promise<any> {
-        if (playerId === "614043079") {
-            playerId = "76561198436848521";
-        } else if (playerId === "1894966344") {
-            playerId = "76561198849650655";
-        }
-
-        try {
-            const response = await fetch(`https://scoresaber.com/api/player/${playerId}/full`);
-            if (response.status === 404) {
-                return {};
-            }
-
-            return await response.json();
-        } catch (e) {
-            console.error(e);
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-            return await this.getScoresaber(playerId);
+            this.socket.send(JSON.stringify({ type, ...data }));
         }
     }
 
